0190996db75ac3facb838050cf12a24efb1c0131
Dmytro Nayda <dima.nayda.1993@gmail.com>
2018-03-03 11:42:33 +0200

    Add slick.js to the jumbotron.

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: true,
+    pauseOnHover: true
+  });
+});
