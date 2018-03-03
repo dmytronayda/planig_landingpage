9c970c2d27a3f898480465c3563f0eb698aea5ac
Dmytro Nayda <dima.nayda.1993@gmail.com>
2018-03-03 11:54:04 +0200

    Remove pause on focus and hover.

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
   $('.carousel').slick({
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 6000,
+    autoplaySpeed: 7000,
     fade: true,
     cssEase: 'linear',
     arrows: false,
     draggable: false,
-    pauseOnFocus: true,
-    pauseOnHover: true
+    pauseOnFocus: false,
+    pauseOnHover: false
   });
 });
